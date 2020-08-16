fdb47aaca41d5d993e0cd90465ce263f4e08f42c
mandyee <minju4144@gmail.com>
2020-08-17 02:16:10 +0900

    #2 Add Manual
    1. 매뉴얼 이미지 파일 추가함
    2. Manual 버튼을 누르면 매뉴얼을 볼 수 있도록 함

--- src/js/contents/ShowAdminManual.js
+++ src/js/contents/ShowAdminManual.js
@@ -6,7 +6,40 @@ class ShowAdminManual extends React.Component {
       <div>
         <div>
           <p class="mb-5">
-            1. ㅇㅇㅇ 버튼을 <strong>클릭</strong>하세요.
+            <h4> <strong> 1. 선거 등록 </strong> </h4>
+            <img src="../../img/add-election.PNG" alt="add-election"/>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 2. 선거 시작 </strong> </h4>
+            <img src="../../img/start-election.PNG" alt="start-election"/>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 3. 선거 종료 </strong> </h4>
+            <img src="../../img/end-election.PNG" alt="end-election"/>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 4. 후보자 등록 </strong> </h4>
+            <img src="../../img/add-candidate-1.PNG" alt="add-candidate"/>
+            <img src="../../img/add-candidate-2.PNG" alt="add-candidate"/>
+          </p>
+
+          <hr/> <br/>
+          <p class="mb-5">
+            <h1> 공통 매뉴얼 </h1>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 1. 투표율 확인 </strong> </h4>
+            <img src="../../img/turnout-1.PNG" alt="turnout"/>
+            <img src="../../img/turnout-2.PNG" alt="turnout"/>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 2. 투표 결과 확인 </strong> </h4>
+            <img src="../../img/results-1.PNG" alt="results"/>
+            <img src="../../img/results-2.PNG" alt="results"/>
+          </p>
+          <p class="mb-5">
+            <h4> <strong> 3. 블록 정보 확인 </strong> </h4>
+            <img src="../../img/blockinfo.PNG" alt="blockinfo"/>
           </p>
         </div>
         <hr/>
